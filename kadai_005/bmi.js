3eaa442b8478992c2336b6352bb330cd18b44b93
Mohri12646 <mugitorodon@gmail.com>
2025-04-23 16:45:28 +0900

    値の代入に書き換えました

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,3 +1,17 @@
 // 体重、身長、BMI（体格指数）
 console.log(1.7*1.7);
-console.log(68 /2.89);
+console.log(68 /2.89);// 変数の宣言
+let weight;
+let height;
+
+// 値の代入
+let weight = '体重';
+let height = '身長';
+
+// 変数の宣言・値の代入
+:let weight = 68;
+:let height = 1.7;
+
+// コンソールへの出力
+console.log(let weight);
+console.log(let height);
