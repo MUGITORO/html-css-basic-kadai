0990a8943290c3da7f89e476e7662440d0573153
Mohri12646 <mugitorodon@gmail.com>
2025-04-27 15:48:11 +0900

    コードを大幅に修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,17 +1,6 @@
-// 体重、身長、BMI（体格指数）
-console.log(1.7*1.7);
-console.log(68 /2.89);// 変数の宣言
-let weight;
-let height;
+let weight = 68;
+let height = 1.7;
 
-// 値の代入
-let weight = '体重';
-let height = '身長';
+let bmi = weight/(height*height);
 
-// 変数の宣言・値の代入
-:let weight = 68;
-:let height = 1.7;
-
-// コンソールへの出力
-console.log(let weight);
-console.log(let height);
+console.log(bmi);
