97ceeaa67f1df2615c1d379d765b7e65c6692702
Mohri12646 <mugitorodon@gmail.com>
2025-05-04 11:26:02 +0900

    セレクタを修正

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -4,13 +4,15 @@ let num = Math.floor(Math.random() * 31);
 // 変数numの値を出力する（確認用）
 console.log(num);
 
+num = 30;
+
 // 変数numの値が3と5の倍数であれば、「3と5の倍数です」という文字列を出力する
-  else if (num % 3 == 0 && num % 5 == 0) {
+  if (num % 3 == 0 && num % 5 == 0) {
     console.log('3と5の倍数です')
 }
 
 // 変数numの値が3の倍数であれば、「3の倍数です」という文字列を出力する
-if (num % 3 == 0) {
+  else if (num % 3 == 0) {
     console.log('3の倍数です')
 }
 
@@ -23,5 +25,3 @@ if (num % 3 == 0) {
    else {
    console.log('num');
 }
-
-num = 30;
