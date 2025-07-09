6c6bbc17b61c11a8ea06f8ad20b6e67f67cc418d
Mohri12646 <mugitorodon@gmail.com>
2025-07-09 14:36:15 +0900

    Update event.js
    全体的に修正

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,7 +1,7 @@
-// output-btnというidを持つHTML要素を取得し、定数に代入する
-const btn = document.getElementById('text');
-
+// HTML要素を取得し、定数に代入する
+document.getElementById('btn');
 // HTML要素がクリックされたときにイベント処理を実行する
-btn.addEventListener('click', () => {
-  console.log('クリックされました！');
+.addEventListener('click', function() => {
+
+  document.getElementById("text").textContent = "クリックしました！";
 });
