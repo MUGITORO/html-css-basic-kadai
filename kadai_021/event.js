6f7021defe9fdae99c82845207283103890e6f39
Mohri12646 <mugitorodon@gmail.com>
2025-07-13 18:06:39 +0900

    課題021をアップロード

---
+++ kadai_021/event.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click', function() {
+
+      // 2秒（2000ミリ秒）後に実行
+      setTimeout(function("text").textContent = "ボタンをクリックしました";
+      }, 2000);
+});
