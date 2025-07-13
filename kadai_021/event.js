943d3acb622637a2099c1d917df131418b37cf75
Mohri12646 <mugitorodon@gmail.com>
2025-07-13 20:38:00 +0900

    Update event.js
    変数textを修正

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -3,7 +3,7 @@ const text = document.getElementById('text');
 
 btn.addEventListener('click', function() {
 
-      // 2秒（2000ミリ秒）後に実行
-      setTimeout(function("text").textContent = "ボタンをクリックしました";
-      }, 2000);
+setTimeout(() => { 
+text.textContent = "ボタンをクリックしました";
+}, 2000);
 });
