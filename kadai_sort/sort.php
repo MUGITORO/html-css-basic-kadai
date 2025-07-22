ae5367bd2dca1e800b043fcf333a4ff4f900185e
Mohri12646 <mugitorodon@gmail.com>
2025-07-22 17:33:51 +0900

    課題「PHPマニュアルを参照してソート関数を実装してみよう」をアップロード

---
+++ kadai_sort/sort.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+
+         // ソートする配列を宣言
+        $nums = [15, 4, 18, 23, 10 ];
+
+            // 昇順にソート
+            sort($nums);
+
+            // 確認のため出力
+            echo '昇順にソートします';
+            echo <br>;
+
+            foreach ($nums as $num) {
+                echo $num . "<br>";
+
+            // 昇順にソート
+            rsort($nums);
+
+            echo '降順にソートします';
+            echo <br>;
+
+            foreach ($nums as $num) {
+                echo $num . "<br>";
+
+        ?>
+    </p>
+</body>
+
+</html>
