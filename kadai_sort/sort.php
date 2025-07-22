bcd0d0c4cb2aa7d4a03c0b7f3ec2b512141c981d
Mohri12646 <mugitorodon@gmail.com>
2025-07-22 23:24:46 +0900

    ソート関数の宣言や引数、呼び出しを修正

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -7,38 +7,49 @@
 </head>
 
 <body>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
     <p>
         <?php
 
-         function sort_2way($array, $order) {
-             if ($order === 'asc') {
-                 sort($array);  // 昇順ソート
-             } elseif ($order === 'desc') {
-                 rsort($array); // 降順ソート
-             } else {
-                 return;
-             }
-
-             return $array;
+        function sort_2way($array, $isAsc) {
+                if ($isAsc) {
+                  sort($array);   // 昇順
+                } else {
+                  rsort($array);  // 降順
+                }
+                return $array;
          }
 
          // ソートする配列を宣言
         $nums = [15, 4, 18, 23, 10 ];
 
-            // 昇順にソート
-            $sortedAsc = sort_2way($nums, 'asc');
-            echo '昇順にソートします';
+           // 昇順でソートして表示
+            $sortedAsc = sort_2way($nums, true);
+            echo "昇順にソートします: ";
+            foreach ($sortedAsc as $num) {
+                echo $num . " ";
+            }
 
-            print_r($sortedAsc);
-
-            // 昇順にソート
-           $sortedDesc = sort_2way($nums, 'desc');
-            echo '降順にソートします';
-
-            print_r($sortedDesc);
+            // 降順でソートして表示
+            $sortedDesc = sort_2way($nums, false);
+            echo "降順にソートします: ";
+            foreach ($sortedDesc as $num) {
+                echo $nums . " ";
+            }
 
         ?>
     </p>
+</body>
+
+</html>
 </body>
 
 </html>
